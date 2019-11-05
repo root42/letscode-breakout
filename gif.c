26d36f83c56b3eac60a70a2405d44fb10784aea7
Arne Schmitz <arne.schmitz@gmx.net>
2019-11-05 22:03:41 +0100

    add most of the gif loading code. TODO: read_code and load_byte

--- gif.c
+++ gif.c
@@ -17,10 +17,93 @@ typedef struct gif_block {
   byte size;
 };
 
+typedef struct decoder_state {
+  FILE gif_file;
+  struct image *img;
+  unsigned int b_pointer;
+  byte buffer[257];
+  byte code_size;
+  char bits_in;
+  byte temp;
+  unsigned int first_free;
+  unsigned int free_code;
+  byte init_code_size;
+  unsigned int code, old_code, max_code;
+  unsigned int clear_code, eoi_code;
+  unsigned int prefix[ 4096 ];
+  byte suffix[ 4096 ];
+  unsigned int x, y;
+  unsigned int tlx, tly, brx, bry, dy;
+};
+
+unsigned int read_code()
+{
+  
+}
+
+void next_pixel( struct decoder_state* decoder, unsigned int c)
+{
+  *(decoder->img->data * decoder->X + decoder->Y * decoder->img->width) = c & 255;
+
+  if( ++(decoder->X) == decoder->brX )
+  {
+    decoder.X = tlX;
+    switch( dY ) {
+    case 0:
+      decoder.Y += 1;
+      break;
+    case 1:
+      decoder.Y += 2;
+      break;
+    case 2:
+      decoder.Y += 4;
+      break;
+    case 3:
+    case 4:
+      decoder.Y += 8;
+      break;
+    }
+    if( Y >= brY ) {
+      switch( --dY ) {
+      case 3:
+	Y = 4;
+	break;
+      case 2:
+	Y = 2;
+	break;
+      case 1:
+	Y = 1;
+	break;
+      }
+    }
+  }
+}
+
+byte out_string( struct *decoder, unsigned int cur_code )
+{
+  unsigned int out_count;
+  byte out_code[1024];
+
+  if( cur_code < decoder->clear_code ) {
+    next_pixel( decoder, cur_code);
+  } else {
+    out_count = 0;
+    do {
+      out_code[out_count++] = decoder->suffix[cur_code];
+      cur_code = decoder->prefix[cur_code];
+    } while( cur_code >= decoder->clear_code );
+    out_code[out_count++] = cur_code;
+    do {
+      next_pixel( out_code[--out_count] );
+    } while( out_count );
+  }
+  return cur_code;
+}
+
 image *load_gif( const char *filename )
 {
   image *img = NULL;
-
+  struct decoder_state decoder;
   struct gif_header header;
   struct gif_descriptor descriptor;
 
@@ -48,19 +131,19 @@ image *load_gif( const char *filename )
   num_of_colors = (1 << bits_per_pixel) - 1;
   if( bits_per_pixel > 1 && bits_per_pixel <= 4 ) {
     bits_per_pixel = 4;
-  } 
+  }
 
   img = malloc( sizeof( image ) );
-  img.width = header.screen_width;
-  img.height = header.screen_height;
-  
+  img->width = header.screen_width;
+  img->height = header.screen_height;
+
   if( header.packed & GIF_FLAG_COLOR_TABLE )
   {
-    fread( img.palette, 3, num_of_colors + 1, gif_file );
+    fread( img->palette, 3, num_of_colors + 1, gif_file );
     for( i = 0; i <= num_of_colors; ++i ) {
-      img.palette[0] >>= 2;
-      img.palette[1] >>= 2;
-      img.palette[2] >>= 2;
+      img->palette[0] >>= 2;
+      img->palette[1] >>= 2;
+      img->palette[2] >>= 2;
     }
   }
 
@@ -76,8 +159,8 @@ image *load_gif( const char *filename )
 
     switch( block.label ) {
     case GIF_EXTENSION_GFXCONTROL:
-      /* skip block: 
-       * packed fields (1) 
+      /* skip block:
+       * packed fields (1)
        * delay time (2)
        * transparent color (1)
        * block terminator (1)
@@ -102,8 +185,73 @@ image *load_gif( const char *filename )
       fseek( gif_file, block.size, SEEK_CUR );
       fseek( gif_file, 1, SEEK_CUR );
       break;
+    }
   }
+
+  fread( &descriptor, sizeof( descriptor ), 1, gif_file );
+
+  if( descriptor.packed & GIF_FLAG_COLOR_TABLE )
+  {
+    fread( img->palette, 3, num_of_colors + 1, gif_file );
+    for( i = 0; i <= num_of_colors; ++i ) {
+      img->palette[0] >>= 2;
+      img->palette[1] >>= 2;
+      img->palette[2] >>= 2;
+    }
+  }
+
+  decoder.tlx = descriptor.image_left;
+  decoder.tly = descriptor.image_top;
+  decoder.brx = tlx + descriptor.image_width;
+  decoder.bry = tly + descriptor.image_height;
+  dy = ( descriptor.packed & GIF_FLAG_INTERLACED ) ? 4 : 0;
+
+  fread( &decoder.code_size, 1, 1, gif_file );
+  fread( &decoder.block_size, 1, 1, gif_file );
+
+  decoder.gif_file = gif_file;
+  decoder.b_pointer = decoder.block_size;
+  decoder.clear_code = 1 << decoder.code_size;
+  decoder.eoi_code = decoder.clear_code + 1;
+  decoder.first_free = decoder.clear_code + 2;
+  decoder.free_code = decoder.first_free;
+  decoder.init_code_size = ++decoder.code_size;
+  decoder.max_code = 1 << decoder.code_size;
+  decoder.bits_in = 8;
+
+  img.x = descriptor.image_left;
+  img.y = descriptor.image_top;
+  decoder.img = &img;
   
+  do {
+    decoder.code = read_code();
+    if( decoder.code == decoder.eoi_code ) {
+      break;
+    } else if( decoder.code == decoder.clear_code ) {
+      decoder.free_code = decoder.first_free;
+      decoder.code_size = decoder.init_code_size;
+      decoder.max_code = 1 << decoder.code_size;
+      decoder.code = read_code();
+      decoder.old_code = decoder.code;
+      next_pixel( &decoder, decoder.code );
+    } else {
+      if( decoder.code < decoder.free_code ) {
+	decoder.suffix[decoder.free_code] = out_string( &decoder, decoder.code );
+      } else {
+	decoder.suffix[decoder.free_code] = out_string( &decoder, decoder.old_code );
+	next_pixel( &decoder, suffix[decoder.free_code]);
+      }
+      decoder.prefix[decoder.free_code++] = decoder.old_code;
+      if( decoder.free_code >= decoder.max_code && decoder.code_size < 12 ) {
+	decoder.code_size++;
+	decoder.max_code <<= 1;
+      }
+      decoder.old_code = decoder.code;
+    }
+  } while( decoder.code != decoder.eoi_code );
+
+  fclose( gif_file );
+
   return img;
 }
 
