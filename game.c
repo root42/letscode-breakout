9fbd5a4f3894d871d79656d2f4d0dead42f0b161
Arne Schmitz <arne.schmitz@gmx.net>
2019-11-10 13:08:15 +0100

    first try at loading -> infinite loop

--- game.c
+++ game.c
@@ -5,6 +5,16 @@
 
 int main()
 {
+  struct image *img;
+  img = load_gif("test.gif");
+  if( img != NULL ) {
+    set_graphics_mode();
+    blit2vga( img->data, 0, 0, 320, 200 );
+    set_text_mode();
+    return 0;
+  } else {
+    printf("Could not load test.gif\n");
+    return 1;
+  }
   
-  return 0;
 }
